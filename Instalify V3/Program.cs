ce2d7a6ea6d60f25280d7bf8f6f110f76072a700
Noco242 <101195050+Noco242@users.noreply.github.com>
2024-07-11 11:48:01 +0200

    -GUI Anpassungen -Suche und Filtern

--- Instalify V3/Program.cs
+++ Instalify V3/Program.cs
@@ -1,11 +1,14 @@
 using Instalify_V3;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<Instalify_V3.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<AppDataService>();
 
 await builder.Build().RunAsync();
